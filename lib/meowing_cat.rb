6609c24d8fb08078e97095108c0e5eaafc8380e2
Samantha Meason <sbmeason@gmail.com>
2020-02-08 20:11:10 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,19 @@
-## code your solution here. 
+class Cat
+  attr_accessor :name
+  
+end
+
+maru = Cat.new
+maru.name = "Maru"
+
+puts maru.name 
+
+class Cat
+  def meow
+    puts "meow!"
+  end
+end
+
+puts maru.meow 
+
+
